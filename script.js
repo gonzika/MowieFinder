29818106e629e97376b3471315160a187f821391
gonzika <hziben@gmail.com>
2021-01-02 14:09:32 +0200

    store

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const URL = 'https://www.omdbapi.com/?apikey='
 const KEY = 'cc66794b'
 btn = document.getElementById('subm')
 result = document.getElementById('results')
+favorLists = document.getElementById('favorList')
 let d
+let storage = []
+let start = 1
 
 document.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -31,14 +34,29 @@ function getFilm(page = 1) {
         .catch((error) => (console.error(error)));
 }
 
+function getInnet(storage, e, i, inner, details) {
+    let classes = "btn-favor"
+    let tit = "Add to your favorite movies"
+    if (localStorage[e.imdbID]) {
+        classes += ' isFavorite'
+        tit = "Delete from your favorite movies"
+    }
+    inner += `<div class="result">
+    <a href="${e.Poster}"><img src="${e.Poster}"></a>
+    <span>${e.Title}</span>
+    <button class="${classes}" id="${e.imdbID}" onclick="addStorage('${e.Title}', '${e.imdbID}', this)" title="${tit}">⭐</button>
+    <button class="btn-details" onclick="writeDetails(${i}, ${storage}, ${details})">Details</button>
+    </div>`
+    return inner
+}
+
 function writeResults(data, page) {
     if (data.Search.length > 0) {
         d = data
         let inner = ''
         data.Search.forEach((e, i) => {
-            inner += `<div><a href="${e.Poster}"><img src="${e.Poster}"></a><span>${e.Title}</span><button onclick="writeDetails(${i})">Details</button></div>`
+            inner = getInnet('d.Search', e, i, inner, 'details')
         });
-        inner += '<br>'
         len = Math.floor(Number(data.totalResults) / 10)
         if (len * 10 > 10) {
             let pagin = ''
@@ -66,17 +84,64 @@ function writeResults(data, page) {
     }
 }
 
-function writeDetails(s) {
+function writeDetails(s, d, details) {
     b = ''
-    b += `<h3>Title: ${d.Search[s].Title}</h3>`
-    b += `<i>Type: ${d.Search[s].Type}</i>`
-    b += `<b>Year: ${d.Search[s].Year}</b>`
-    b += `<div>imdbID: ${d.Search[s].imdbID}</div>`
+    let imdb = d[s].imdbID
+    b += `<h3>Title: ${d[s].Title} </h3>`
+    b += `<div><i>Type: ${d[s].Type}</i></div>`
+    b += `<div><b>Year: ${d[s].Year}</b></div>`
+    b += `<div>imdbID: ${imdb}</div>`
     details.innerHTML = b
 }
 
 function eventer(event, t) {
     if (event.keyCode === 13) {
         getFilm(t.value)
+    }
+}
+
+function addStorage(t, i, th) {
+    if (localStorage[i]) {
+        localStorage.removeItem(i)
+        th.classList.remove('isFavorite')
+    } else {
+        localStorage.setItem(i, t)
+        th.classList.add('isFavorite')
+    }
+}
+
+function watchFavorite(s, h) {
+    if (s == 'favorite') doFavoriteList()
+    let hid = document.querySelector('.' + h)
+    let show = document.querySelector('.' + s)
+    hid.classList.add("hidden");
+    show.classList.remove("hidden");
+    detailsF.innerHTML = ''
+    details.innerHTML = ''
+}
+
+function doFavoriteList() {
+    let innerF = ''
+    let i = 0
+    favorLists.innerHTML = ''
+    for (key in localStorage) {
+        if (key[0] == 't' && key[1] == 't') {
+            let req = `${URL}${KEY}&i=${key}`
+            fetch(req)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    if (data.Error) {
+                        console.error(data.Error);
+                    } else {
+                        console.log(data);
+                        storage.push(data)
+                        favorLists.innerHTML += getInnet('storage', data, i, innerF, 'detailsF')
+                        i++
+                    }
+                })
+                .catch((error) => (console.error(error)));
+        }
     }
 }
